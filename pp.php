ac03e347438e2da056118c62ed0d7570ce1e5bce
suwatthana <Suwatthana.wo@digitalbusinessconsult.asia>
2022-12-09 16:01:16 +0700

    first commit

---
+++ pp.php
@@ -0,0 +1,5 @@
+<?php
+include("pp1.php");
+echo"ราชบัญญัติคุ้มครองข้อมูลส่วนบุคคล พ.ศ. ๒๕๖๒";
+echo "<h1>PDPA Thailand</h>"
+?> 
---
+++ pp1.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>PDPA Thailand</h1>
+
+<?php
+echo  "https://pdpa.thailandpdpa.com/";
+?> 
+
+</body>
+</html>
